ab7f764968999435b2dad7fff60c61b2be7303f9
Evgeniy Yurchak <zhenyaa2007@gmail.com>
2021-04-28 21:56:07 +0300

    edit

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,12 @@
-let inputVal = document.getElementById("validation-input");
-
-let totalLenght = inputVal.getAttribute("data-length");
-let intTotallenght = parseInt(totalLenght, 10);
-
-inputVal.oninput = function() {
-  if (inputVal.value.length === intTotallenght) {
-    inputVal.classList.remove("invalid");
-    inputVal.classList.add("valid");
-  }
-  if (inputVal.value.length === 0) {
-    inputVal.classList.remove("valid");
-    inputVal.classList.remove("invalid");
-  }
-  if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
-    inputVal.classList.add("invalid");
-  }
+let inputVal = document.getElementById('validation-input');
+let totalLength = inputVal.getAttribute('data-length');
+let intTotalLength = parseInt(totalLength, 10);
+inputVal.oninput = function () {
+    if (inputVal.value.length === intTotalLength) {
+        inputVal.classList.remove('invalid');
+        inputVal.classList.add('valid');
+        return;
+    }
+    inputVal.classList.remove('valid');
+    inputVal.classList.add('invalid');
 };
